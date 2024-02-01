6be714bf7f90113cc7578954611fa8e6cce65301
welli7ngton <wellingtonasilva45@gmail.com>
2024-02-01 10:32:01 -0300

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+
+from PySide6.QtWidgets import QApplication
+from src.ui import MainWindow
+import sys
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec())
+
+
+main()
