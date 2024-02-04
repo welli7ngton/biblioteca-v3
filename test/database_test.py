d42f6c8f0e5ef03b71a4e3f1b325dc92edf4d9d6
welli7ngton <wellingtonasilva45@gmail.com>
2024-02-04 14:38:27 -0300

    refactor: um jeito mais simples de fazer a limpeza no banco de dados.

--- test/database_test.py
+++ test/database_test.py
@@ -2,8 +2,7 @@
 import sqlite3
 import os
 
-
-class DataBaseTest:
+class DataBaseTest():
     CONNECTION = os.getenv('DATABASE_PATH_TEST')
 
     def __init__(self) -> None:
@@ -25,20 +24,17 @@ class DataBaseTest:
         conn.commit()
         conn.close()
 
-    def __delete_db(self):
+    def _delete_db(self):
         conn = sqlite3.connect(self.CONNECTION)
         cursor = conn.cursor()
-        with open('scripts/sql/delete_all_data_students.sql', 'r') as file1:
-            script1 = file1.read()
 
-        with open('scripts/sql/delete_all_data_books.sql', 'r') as file2:
-            script2 = file2.read()
-
-        with open('scripts/sql/delete_all_data_loan.sql', 'r') as file3:
-            script3 = file3.read()
-
-        cursor.executescript(script1)
-        cursor.executescript(script2)
-        cursor.executescript(script3)
+        for db in ['students', 'books', 'loan']:
+            cursor.execute(
+                f'DELETE FROM {db};'
+            )
         conn.commit()
         conn.close()
+
+if __name__ == '__main__':
+    db = DataBaseTest()
+    db._delete_db()
